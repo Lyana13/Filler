43e7d3a66e7bf8ed2aaf3377540a82c2f8fe5657
Lyana13 <malaya95lyana@gmail.com>
2019-07-18 23:57:36 +0300

    jj

--- filler.c
+++ filler.c
@@ -31,11 +31,39 @@ void	create_board(char *str, t_filler *state)
 		state->board.columns) * sizeof(t_cell));
 }
 
+void fill_board(t_filler *state)
+{
+	int count;
+	int i;
+	int j;
+	char 		*str;
+
+	get_next_line(0, &str);
+	free(str);
+	i = 0;
+	j = 0;
+	while (j < state->board.rows)
+	{
+		count = 4;
+		get_next_line(0, &str);
+		while (count < state->board.columns + 4)
+		{
+			state->board.cells[i].symbol = str[count];
+			i++;
+			count++;
+		}
+		//free(str);
+		j++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	t_filler	state;
 	t_position	position;
 	char 		*str;
+	int i = 0;
+
 	
 	fptr = fopen("./test.txt","w");
 	(void)argc;
@@ -44,12 +72,16 @@ int main(int argc, char **argv)
 	while (get_next_line(0, &str))
 	{
 	 	if (state.board.cells == NULL)
-			create_board(str, &state); 
-		
-	 	fprintf(fptr, "%s\n", str);
+			create_board(str, &state);
 	 	free(str);	
+	 	fill_board(&state);
+	 	while(i < state.board.rows * state.board.columns){
+	 		fprintf(fptr, "%c", state.board.cells[i].symbol);
+	 	i++;
+	 	}
+	 	//fprintf(fptr, "%s\n", str);
 	}
-	fprintf(fptr, "roww:%d, col: %d\n", state.board.rows, state.board.columns);
+	fprintf(fptr, "roww:%d, col: %d, dol: %c\n", state.board.rows, state.board.columns, state.board.cells[i].symbol);
 	//free(str);
 	fclose(fptr);
 	return (0);
